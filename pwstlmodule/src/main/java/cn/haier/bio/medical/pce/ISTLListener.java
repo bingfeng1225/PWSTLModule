bb69d395002fda99a54a12894d715934b32f2f4c
bang.liu <bang.liu@haierbiomedical.com>
2021-03-31 18:21:43 +0800

    Fix package received callback

--- pwstlmodule/src/main/java/cn/haier/bio/medical/pce/ISTLListener.java
+++ pwstlmodule/src/main/java/cn/haier/bio/medical/pce/ISTLListener.java
@@ -5,5 +5,5 @@ public interface ISTLListener {
     void onSTLConnected();
     void onSTLPrint(String message);
     void onSTLException(Throwable throwable);
-    byte[] onSTLPackageReceived(byte[] data);
+    void onSTLPackageReceived(byte[] data);
 }
